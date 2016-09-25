1f5b6fccb20ac9cb6ba4cc09a72fc1f13cbace5a
brun7695 <brun7695@stthomas.edu>
2016-09-25 00:20:47 +0000

    Hello World

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-      render html "We are ready"
+      render html: "We are ready"
   end
 end
